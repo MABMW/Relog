8effc2e05756b045e4aa6b8ca0846301cadc4760
MABMW <mario555beltran@hotmail.com>
2020-09-06 18:26:11 -0600

    initialize Commit

---
+++ Relog/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace Relog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.DesperMatu = new System.Windows.Forms.Label();
+            this.lblfecha = new System.Windows.Forms.Label();
+            this.lblmes = new System.Windows.Forms.Label();
+            this.circularProgressBar1 = new CircularProgressBar.CircularProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label1.Location = new System.Drawing.Point(61, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 63);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "--:--";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label2.Location = new System.Drawing.Point(211, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "--";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.panel1.Controls.Add(this.DesperMatu);
+            this.panel1.Controls.Add(this.lblfecha);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.lblmes);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(15, 122);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(261, 124);
+            this.panel1.TabIndex = 2;
+            // 
+            // DesperMatu
+            // 
+            this.DesperMatu.AutoSize = true;
+            this.DesperMatu.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DesperMatu.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.DesperMatu.Location = new System.Drawing.Point(37, 37);
+            this.DesperMatu.Name = "DesperMatu";
+            this.DesperMatu.Size = new System.Drawing.Size(15, 13);
+            this.DesperMatu.TabIndex = 6;
+            this.DesperMatu.Text = "--";
+            // 
+            // lblfecha
+            // 
+            this.lblfecha.AutoSize = true;
+            this.lblfecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblfecha.ForeColor = System.Drawing.Color.SeaGreen;
+            this.lblfecha.Location = new System.Drawing.Point(158, 77);
+            this.lblfecha.Name = "lblfecha";
+            this.lblfecha.Size = new System.Drawing.Size(52, 25);
+            this.lblfecha.TabIndex = 5;
+            this.lblfecha.Text = "-----";
+            // 
+            // lblmes
+            // 
+            this.lblmes.AutoEllipsis = true;
+            this.lblmes.AutoSize = true;
+            this.lblmes.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblmes.ForeColor = System.Drawing.Color.SeaGreen;
+            this.lblmes.Location = new System.Drawing.Point(67, 77);
+            this.lblmes.Name = "lblmes";
+            this.lblmes.Size = new System.Drawing.Size(68, 25);
+            this.lblmes.TabIndex = 4;
+            this.lblmes.Text = "-------";
+            // 
+            // circularProgressBar1
+            // 
+            this.circularProgressBar1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.circularProgressBar1.AnimationFunction = WinFormAnimation.KnownAnimationFunctions.CircularEaseIn;
+            this.circularProgressBar1.AnimationSpeed = 500;
+            this.circularProgressBar1.BackColor = System.Drawing.Color.Transparent;
+            this.circularProgressBar1.Font = new System.Drawing.Font("Microsoft Sans Serif", 70F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.circularProgressBar1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.circularProgressBar1.InnerColor = System.Drawing.Color.Transparent;
+            this.circularProgressBar1.InnerMargin = 1;
+            this.circularProgressBar1.InnerWidth = -1;
+            this.circularProgressBar1.Location = new System.Drawing.Point(155, 1);
+            this.circularProgressBar1.MarqueeAnimationSpeed = 2000;
+            this.circularProgressBar1.Maximum = 60;
+            this.circularProgressBar1.Name = "circularProgressBar1";
+            this.circularProgressBar1.OuterColor = System.Drawing.Color.Blue;
+            this.circularProgressBar1.OuterMargin = -25;
+            this.circularProgressBar1.OuterWidth = 26;
+            this.circularProgressBar1.ProgressColor = System.Drawing.Color.Gainsboro;
+            this.circularProgressBar1.ProgressWidth = 15;
+            this.circularProgressBar1.SecondaryFont = new System.Drawing.Font("Microsoft Sans Serif", 36F);
+            this.circularProgressBar1.Size = new System.Drawing.Size(121, 115);
+            this.circularProgressBar1.StartAngle = 270;
+            this.circularProgressBar1.Step = 1;
+            this.circularProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.circularProgressBar1.SubscriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(166)))), ((int)(((byte)(166)))));
+            this.circularProgressBar1.SubscriptMargin = new System.Windows.Forms.Padding(10, -35, 0, 0);
+            this.circularProgressBar1.SubscriptText = "";
+            this.circularProgressBar1.SuperscriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(166)))), ((int)(((byte)(166)))));
+            this.circularProgressBar1.SuperscriptMargin = new System.Windows.Forms.Padding(10, 35, 0, 0);
+            this.circularProgressBar1.SuperscriptText = "";
+            this.circularProgressBar1.TabIndex = 3;
+            this.circularProgressBar1.TextMargin = new System.Windows.Forms.Padding(8, 8, 0, 0);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Font = new System.Drawing.Font("Wingdings 2", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label3.Location = new System.Drawing.Point(184, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 50);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "È";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.circularProgressBar1);
+            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Location = new System.Drawing.Point(8, 33);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(291, 254);
+            this.panel2.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(62, 46);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Decir Hora";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(71, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 46);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Decir Fecha";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 297);
+            this.Controls.Add(this.panel2);
+            this.DisplayHeader = false;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(20, 30, 20, 20);
+            this.Resizable = false;
+            this.Text = "Relog";
+            this.TransparencyKey = System.Drawing.Color.SkyBlue;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private CircularProgressBar.CircularProgressBar circularProgressBar1;
+        private System.Windows.Forms.Label lblmes;
+        private System.Windows.Forms.Label lblfecha;
+        private System.Windows.Forms.Label DesperMatu;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Relog/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CircularProgressBar;
+using System.Speech.Synthesis;
+using System.Threading;
+namespace Relog
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        //evento Tick de contador de tiempo
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //darle el formato para mostrar la hora y la fecha
+            label1.Text = DateTime.Now.ToString("hh:mm");
+            label2.Text = DateTime.Now.ToString("ss");
+            lblfecha.Text = DateTime.Now.ToString("M-d");
+            lblmes.Text = DateTime.Now.ToString("ddddd");
+
+            circularProgressBar1.Value++;
+            /*
+             * el siguiente if nos sirve para reiniciar nuestro 
+             * circularprogressbar. ya que hay que configurar el
+             * maximo del  value en 60 ya que son los segundos los que se cuentan
+             */
+            if (circularProgressBar1.Value.Equals(60))
+            {
+                circularProgressBar1.Value = 0;
+            }
+
+            //este if sirve para configurar si es turno matutino o vespertino
+            if (DateTime.Now.Hour>=12)
+            {
+                DesperMatu.Text = "PM";
+
+            }
+            else if(DateTime.Now.Hour<=11){
+                DesperMatu.Text = "AM";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //establecer el valor del cicularprogressbar  igual que los segundos
+            circularProgressBar1.Value = DateTime.Now.Second;
+        }
+
+        //metodo de configuracion de voz
+        private void hablar(object text)
+        {
+            SpeechSynthesizer voz = new SpeechSynthesizer();
+            voz.SetOutputToDefaultAudioDevice();
+            voz.Rate = -3;
+            voz.Volume = 100;
+            voz.Speak(text.ToString());
+        }
+
+        //evento para decir la hora y los minutos actuales
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string hor = DateTime.Now.ToString("h:");
+            string min = DateTime.Now.ToString("m:");
+            Thread tar = new Thread(new ParameterizedThreadStart(hablar));
+            tar.Start("La hora es!" + hor+"horas con"+min+"minutos."+ DesperMatu.Text);
+
+        }
+
+        //evento click para decir la fecha
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string fecha = DateTime.Now.ToString("d:");
+            string fecha1 = DateTime.Now.ToString("MMMM:");
+            string a = DateTime.Now.ToString("yyyy");
+            Thread tar = new Thread(new ParameterizedThreadStart(hablar));
+            tar.Start("Hoy es!" + lblmes.Text + fecha+"de"+fecha1+"de"+a);
+        }
+
+        /*
+         * Librerias externas ocupadas es este proyecto: CircularProgressbar y MetroFramework
+         */
+    }
+}
